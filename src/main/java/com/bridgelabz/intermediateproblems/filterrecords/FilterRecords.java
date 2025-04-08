ed833d64e8b8f9dce9e0ff12ab03636af3903919
dgarg7599 <gargdivyansh7599@gmail.com>
2025-04-08 13:48:39 +0530

    [Divyansh Garg] ADD: Filter records from CSV file.

--- src/main/java/com/bridgelabz/intermediateproblems/filterrecords/FilterRecords.java
+++ src/main/java/com/bridgelabz/intermediateproblems/filterrecords/FilterRecords.java
@@ -1,25 +1,46 @@
 package com.bridgelabz.intermediateproblems.filterrecords;
 
+import com.opencsv.CSVReader;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 
 public class FilterRecords {
     public static void main(String[] args) {
         String filePath = "D:\\bridgelabz-workspace\\java-csv-data-handling\\src\\main\\java\\com\\bridgelabz\\intermediateproblems\\filterrecords\\studentData.csv";
-        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
-            String line;
 
-            br.readLine(); // skip header
+//        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+//            String line;
+//
+//            br.readLine(); // skip header
+//
+//            while((line = br.readLine()) != null){
+//                String[] fields = line.split(",");
+//                double marks = Double.parseDouble(fields[3]);
+//                if(marks>80){
+//                    System.out.println(line);
+//                }
+//            }
+//        }catch(Exception e){
+//            e.printStackTrace();
+//        }
 
-            while((line = br.readLine()) != null){
-                String[] fields = line.split(",");
-                double marks = Double.parseDouble(fields[3]);
+        try(CSVReader reader = new CSVReader(new FileReader(filePath))){
+
+            List<String[]> allRows = reader.readAll(); // read all rows at once
+            // start from index 1 to skip header
+            for(int i=1;i<allRows.size();i++){
+                String[] row = allRows.get(i);
+                double marks = Double.parseDouble(row[3]);
+
                 if(marks>80){
-                    System.out.println(line);
+                    System.out.println(String.join(", ",row));
                 }
             }
-        }catch(IOException e){
+
+        }catch(Exception e){
             e.printStackTrace();
         }
     }
